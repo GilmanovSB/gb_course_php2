ca2c517c42c83c7b7f00af93e70b64fc01aa8bdb
Gilmanov Sergey <gilmanov10@outlook.ru>
2020-11-08 22:42:03 +0300

    add ex.1

--- public/index.php
+++ public/index.php
@@ -1,2 +1,15 @@
 <?php
-echo 'hello, world!!!';
+require '../settings/settings.php';
+require ENGINE_DIR . 'Goods.php';
+require ENGINE_DIR . 'Sale.php';
+
+$product = new Goods('Apple iPhone XII','This is new iPhone 2020','1000', 'img/iphone.jpg');
+$products[] = $product->getGoods();
+
+$product = new Sale('Apple iPhone XII','This is new iPhone 2020','1000', 'img/iphone_sale.jpg', 0.8);
+$products[] = $product->getGoods();
+
+require VIEWS_DIR . 'view.php';
+?>
+
+
